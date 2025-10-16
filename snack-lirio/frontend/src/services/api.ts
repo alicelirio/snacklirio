a6f4af05162ebd7216a55da6dcc0eb1ecf2a204a
Henrique Costa <henriquefcosta19@gmail.com>
2025-10-16 11:23:45 -0300

    banco de dados funcionando no alwaysdata

--- snack-lirio/frontend/src/services/api.ts
+++ snack-lirio/frontend/src/services/api.ts
@@ -1,16 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 const api = axios.create({ baseURL });
 
 // Interceptor para adicionar o token em todas as requisições
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('snacklirio:token');
-  
+  const token = localStorage.getItem("snacklirio:token");
+
   if (token && config.headers) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-  
+
   return config;
 });
 
